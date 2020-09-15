38dde4bea70e8289031c4d496bd1056b3e65eb70
fightwing <2608379678@qq.com>
2020-09-15 19:45:19 +0800

    重构了长方法

--- 1/src/main/java/com/twu/refactoring/OrderReceipt.java
+++ 1/src/main/java/com/twu/refactoring/OrderReceipt.java
@@ -14,19 +14,9 @@ public class OrderReceipt {
         this.o = o;
 	}
 
-	public String printReceipt() {
-		StringBuilder output = new StringBuilder();
+	StringBuilder output = new StringBuilder();
 
-		// print headers
-		output.append("======Printing Orders======\n");
-
-		// print date, bill no, customer name
-//        output.append("Date - " + order.getDate();
-        output.append(o.getCustomerName());
-        output.append(o.getCustomerAddress());
-//        output.append(order.getCustomerLoyaltyNumber());
-
-		// prints lineItems
+    public void getLineItems() {
 		double totSalesTx = 0d;
 		double tot = 0d;
 		for (LineItem lineItem : o.getLineItems()) {
@@ -38,20 +28,19 @@ public class OrderReceipt {
 			output.append('\t');
 			output.append(lineItem.totalAmount());
 			output.append('\n');
-
-			// calculate sales tax @ rate of 10%
-            double salesTax = lineItem.totalAmount() * .10;
-            totSalesTx += salesTax;
-
-            // calculate total amount of lineItem = price * quantity + 10 % sales tax
-            tot += lineItem.totalAmount() + salesTax;
+			double salesTax = lineItem.totalAmount() * .10;
+			totSalesTx += salesTax;
+			tot += lineItem.totalAmount() + salesTax;
+			output.append("Sales Tax").append('\t').append(totSalesTx);
+			output.append("Total Amount").append('\t').append(tot);
 		}
 
-		// prints the state tax
-		output.append("Sales Tax").append('\t').append(totSalesTx);
-
-        // print total amount
-		output.append("Total Amount").append('\t').append(tot);
+	}
+	public String printReceipt() {
+		output.append("======Printing Orders======\n");
+		output.append(o.getCustomerName());
+		output.append(o.getCustomerAddress());
+		getLineItems();
 		return output.toString();
 	}
 }
