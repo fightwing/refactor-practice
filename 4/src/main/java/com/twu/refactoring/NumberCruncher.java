423d7ced3b691a5dec8b1cbf158cbc647c9e6f94
fightwing <2608379678@qq.com>
2020-09-15 21:06:05 +0800

    抽取公共方法

--- 4/src/main/java/com/twu/refactoring/NumberCruncher.java
+++ 4/src/main/java/com/twu/refactoring/NumberCruncher.java
@@ -1,5 +1,8 @@
 package com.twu.refactoring;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NumberCruncher {
     private final int[] numbers;
 
@@ -7,35 +10,40 @@ public class NumberCruncher {
         this.numbers = numbers;
     }
 
-    public int countEven() {
-        int count = 0;
+    public List<Integer> count(){
+        int evenCount, oddCount, positiveCount, negativeCount;
+        evenCount = oddCount = positiveCount = negativeCount =0;
+        List<Integer> countList =new ArrayList<>();
         for (int number : numbers) {
-            if (number % 2 == 0) count++;
+            if (number % 2 == 0) evenCount++;
+            if (number % 2 == 1) oddCount++;
+            if (number >= 0) positiveCount++;
+            if (number < 0) negativeCount++;
         }
+        countList.add(evenCount);
+        countList.add(oddCount);
+        countList.add(positiveCount);
+        countList.add(negativeCount);
+        return countList;
+    }
+
+    public int countEven() {
+        int count = count().get(0);
         return count;
     }
 
     public int countOdd() {
-        int count = 0;
-        for (int number : numbers) {
-            if (number % 2 == 1) count++;
-        }
+        int count = count().get(1);
         return count;
     }
 
     public int countPositive() {
-        int count = 0;
-        for (int number : numbers) {
-            if (number >= 0) count++;
-        }
+        int count = count().get(2);
         return count;
     }
 
     public int countNegative() {
-        int count = 0;
-        for (int number : numbers) {
-            if (number < 0) count++;
-        }
+        int count = count().get(3);
         return count;
     }
 }
